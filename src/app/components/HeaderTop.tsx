16a64b91512e537f18049cc90834024877fa5a45
UdithDev <daveudith@gmail.com>
2023-12-11 12:07:53 +0530

    add header top to social media icons

--- src/app/components/HeaderTop.tsx
+++ src/app/components/HeaderTop.tsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import React from "react";
+import { FaSquareFacebook } from "react-icons/fa6";
+import { FaInstagram, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 
 const HeaderTop = () => {
   return (
-    <div className='border-b border-gray-200 hidden sm:block'>HeaderTop
-    <div className='container py-4'>
-        <div className='flex justify-between items-center'>
-          <div className='hidden lg:flex gap-1'>
-            <div className='header_top_icon_wrapper'></div>
-
+    <div className="border-b border-gray-200 hidden sm:block">
+      <div className="container py-4">
+        <div className="flex justify-between items-center">
+          <div className="hidden lg:flex gap-1">
+            <div className="header_top_icon_wrapper">
+              <FaSquareFacebook />
+            </div>
+            <div className="header_top_icon_wrapper">
+              <FaTwitter />
+            </div>
+            <div className="header_top_icon_wrapper">
+              <FaInstagram />
+            </div>
+            <div className="header_top_icon_wrapper">
+              <FaLinkedinIn />
+            </div>
           </div>
         </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default HeaderTop
+export default HeaderTop;
