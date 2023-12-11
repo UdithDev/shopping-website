31276ab170245e6edb1bb1d68f406d0fde320952
UdithDev <daveudith@gmail.com>
2023-12-11 11:45:57 +0530

    first commit

---
+++ src/app/components/HeaderTop.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HeaderTop = () => {
+  return (
+    <div className='border-b border-gray-200 hidden sm:block'>HeaderTop
+    <div className='container py-4'>
+        <div className='flex justify-between items-center'>
+          <div className='hidden lg:flex gap-1'>
+            <div className='header_top_icon_wrapper'></div>
+
+          </div>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default HeaderTop
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      container:{
+        center:true,
+        padding:"15px"
+      },
+
+      colors:{
+        accent:"#FF8F9C",
+        backish:"#1b1b1b"
+      }
+    },
+  },
+  plugins: [],
+}
+
